f638b654ec08942e8baf302a84098b305bcd4925
Thiago Patricio <thiagopatriciod@gmail.com>
2019-09-04 22:12:46 -0300

    Chore: Alterando mensagem na rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/teste'), (req, res) => {
-  return res.json({hello: 'Wolrd'})
+  return res.json({hello: 'Wolrd2'})
 });
 
 app.listen(3333);
